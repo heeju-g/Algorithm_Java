1614522f117fa88e89010aef1ba67cca1795c346
Heeju Sim <heeju01226@gmail.com>
2021-10-21 19:51:01 +0900

    주식투자

--- src/cos/Invest.java
+++ src/cos/Invest.java
@@ -1,5 +1,30 @@
 package cos;
 
 public class Invest {
+	   int solution(int[] prices){
+			  
+	        int inf = 1000000001;
+	        int tmp = inf;
+	        int answer = -inf;
+	        for(int price : prices){
+	            if(tmp != inf)
+	                answer = Math.max(answer, price - tmp);
+	            tmp = Math.min(tmp, price);
+	        }
+	        return answer;
+	    }
+	   public static void main(String[] args) {
+	        Invest sol = new Invest();
+	        int[] prices1 = {1, 2, 3};
+	        int ret1 = sol.solution(prices1);
+	        
+	        // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+	        System.out.println("solution 메소드의 반환 값은 " + ret1 + " 입니다.");
 
+	        int[] prices2 = {3, 1};
+	        int ret2 = sol.solution(prices2);
+	        
+	        // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+	        System.out.println("solution 메소드의 반환 값은 " + ret2 + " 입니다.");
+	    }
 }
