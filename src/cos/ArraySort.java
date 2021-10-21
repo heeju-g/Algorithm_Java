1567ea1aae11dd7163493dd5f3106a47a1e216b0
Heeju Sim <heeju01226@gmail.com>
2021-10-21 16:31:54 +0900

    정렬, 병합

--- src/cos/ArraySort.java
+++ src/cos/ArraySort.java
@@ -9,14 +9,17 @@ public class ArraySort {
         int arrB_len = arrB.length;
         int answer[] = new int[arrA_len + arrB_len];
         int answer_idx = 0;
+        //ºóÄ­1
         while(arrA_idx<arrA_len && arrB_idx<arrB_len){
             if(arrA[arrA_idx] < arrB[arrB_idx])
                 answer[answer_idx++] = arrA[arrA_idx++];
             else
                 answer[answer_idx++] = arrB[arrB_idx++];
         }
+      //ºóÄ­2
         while(arrA_idx<arrA_len)
             answer[answer_idx++] = arrA[arrA_idx++];
+      //ºóÄ­3
         while(arrB_idx<arrB_len)
             answer[answer_idx++] = arrB[arrB_idx++];
         return answer;
