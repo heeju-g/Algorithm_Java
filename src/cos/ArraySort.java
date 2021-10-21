ed4da66051481fed2039295425b70adb4c2c26d7
Heeju Sim <heeju01226@gmail.com>
2021-10-21 16:31:14 +0900

    정렬, 병합

---
+++ src/cos/ArraySort.java
@@ -0,0 +1,33 @@
+package cos;
+
+import java.util.Arrays;
+
+public class ArraySort {
+    public int[] solution(int[] arrA, int[] arrB) {
+        int arrA_idx = 0, arrB_idx = 0;
+        int arrA_len = arrA.length;
+        int arrB_len = arrB.length;
+        int answer[] = new int[arrA_len + arrB_len];
+        int answer_idx = 0;
+        while(arrA_idx<arrA_len && arrB_idx<arrB_len){
+            if(arrA[arrA_idx] < arrB[arrB_idx])
+                answer[answer_idx++] = arrA[arrA_idx++];
+            else
+                answer[answer_idx++] = arrB[arrB_idx++];
+        }
+        while(arrA_idx<arrA_len)
+            answer[answer_idx++] = arrA[arrA_idx++];
+        while(arrB_idx<arrB_len)
+            answer[answer_idx++] = arrB[arrB_idx++];
+        return answer;
+    }
+    public static void main(String[] args) {
+        ArraySort sol = new ArraySort();
+        int[] arrA = {-2, 3, 5, 9};
+        int[] arrB = {0, 1, 5};
+        int[] ret = sol.solution(arrA, arrB);
+ 
+        // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+        System.out.println("solution 메소드의 반환 값은 " + Arrays.toString(ret) + " 입니다.");
+    }
+}
