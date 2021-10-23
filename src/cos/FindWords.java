d7236512f559ec253c19fdbd4727f12cda08c0b3
Heeju Sim <heeju01226@gmail.com>
2021-10-23 15:46:02 +0900

    단어조합

---
+++ src/cos/FindWords.java
@@ -0,0 +1,43 @@
+package cos;
+
+import java.util.ArrayList;
+
+public class FindWords {
+    String[] vowels = {"A", "E", "I", "O", "U"};
+    ArrayList<String> words;
+    public void create_words(int lev, String str) {
+        words.add(str);
+        for (int i = 0; i < 5; i++) {
+            if (lev < 5) {
+                create_words(lev+1, str.concat(vowels[i]));
+            }
+        }
+    }
+    
+    public int solution(String word) {
+        int answer = 0;
+        words  = new ArrayList<String>();
+        create_words(0, "");
+        for (int i = 0; i < words.size(); i++) {
+            if (word.equals(words.get(i))) {
+                answer = i;
+                break;
+            }
+        }
+        return answer;
+    }
+    public static void main(String[] args) {
+        FindWords sol = new FindWords();
+        String word1 = new String("AAAAE");
+        int ret1 = sol.solution(word1);
+
+        // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+        System.out.println("solution 메소드의 반환 값은 " + ret1 + " 입니다.");
+
+        String word2 = new String("AAAE");
+        int ret2 = sol.solution(word2);
+
+        // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+        System.out.println("solution 메소드의 반환 값은 " + ret2 + " 입니다.");
+    }
+}
